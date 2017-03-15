65aae51645398c5250f282d366b397879830d784
seoin <inguk920@naver.com>
2017-03-15 20:04:26 +0900

    seo test 2

--- app/src/main/java/mj/bigbebig/Activity/Infobook.java
+++ app/src/main/java/mj/bigbebig/Activity/Infobook.java
@@ -15,7 +15,7 @@ public class Infobook extends Activity{
     AnimationDrawable ani;
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_infobook);
         img = (ImageView)findViewById(R.id.infoanim);
         ani = (AnimationDrawable)img.getDrawable();
         ani.setOneShot(true);
