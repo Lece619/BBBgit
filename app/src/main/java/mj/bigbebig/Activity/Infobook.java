64c81e171722662b844545675d5f1950d66c80f3
seoin <inguk920@naver.com>
2017-03-15 22:09:07 +0900

    animation end ok

--- app/src/main/java/mj/bigbebig/Activity/Infobook.java
+++ app/src/main/java/mj/bigbebig/Activity/Infobook.java
@@ -33,8 +33,8 @@ public class Infobook extends Activity{
                     @Override
                     public void run() {
                         drawable.stop();
-                        Button a = (Button)findViewById(R.id.btn_character);
-                        a.setVisibility(View.VISIBLE);
+                        Button btn_char = (Button)findViewById(R.id.btn_character);
+                        btn_char.setVisibility(View.VISIBLE);
                     }
                 });
             }
