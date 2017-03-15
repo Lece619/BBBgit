1ee27afc331e0d88ef1823a8691c86e1785ca124
minkjee92 <mk92@hanmail.net>
2017-03-15 21:32:14 +0900

    minkjee92

--- app/src/main/java/mj/bigbebig/Activity/MainActivity.java
+++ app/src/main/java/mj/bigbebig/Activity/MainActivity.java
@@ -26,7 +26,7 @@ import mj.bigbebig.R;
  *  Test123213
  * seo test2
  * jino112213
- * asdfasdfasdfsadfsadfasdfawefascasef
+ * mk92mk92mk92mk92mk92mk92mk92mk92mk92
  */
 
 public class MainActivity extends Activity {
