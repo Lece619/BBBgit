65deb4f67671caec859a9d38ab83436cc6808131
seoin <inguk920@naver.com>
2017-03-15 20:58:51 +0900

    animation good

--- app/src/main/java/mj/bigbebig/Activity/Infobook.java
+++ app/src/main/java/mj/bigbebig/Activity/Infobook.java
@@ -3,6 +3,8 @@ package mj.bigbebig.Activity;
 import android.app.Activity;
 import android.graphics.drawable.AnimationDrawable;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 
 import mj.bigbebig.R;
@@ -11,15 +13,16 @@ import mj.bigbebig.R;
  * Created by Seo on 2017-03-14.
  */
 public class Infobook extends Activity{
-    ImageView img;
-    AnimationDrawable ani;
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_infobook);
-        img = (ImageView)findViewById(R.id.infoanim);
-        ani = (AnimationDrawable)img.getDrawable();
-        ani.setOneShot(true);
+        ImageView iv = (ImageView)findViewById(R.id.infoanim);
+
+        final AnimationDrawable drawable =
+                (AnimationDrawable) iv.getBackground();
+        drawable.start();
+        drawable.setOneShot(true);
 
     }
-
 }
+
