0b08ba0dcfe6b627fe2c9110e597cae54dabaed7
JinHo <lece619@gmail.com>
2017-03-17 13:58:20 +0900

    Commit!

--- app/src/main/java/mj/bigbebig/Activity/Infobook.java
+++ app/src/main/java/mj/bigbebig/Activity/Infobook.java
@@ -5,9 +5,6 @@ import android.graphics.drawable.AnimationDrawable;
 import android.os.Bundle;
 import android.os.Handler;
 import android.view.View;
-import android.view.animation.Animation;
-import android.view.animation.AnimationSet;
-import android.view.animation.AnimationUtils;
 import android.widget.Button;
 import android.widget.ImageView;
 
@@ -15,7 +12,6 @@ import mj.bigbebig.R;
 
 /**
  * Created by Seo on 2017-03-14.
- * ㅎㅇ
  */
 public class Infobook extends Activity{
     Handler handler = new Handler();
