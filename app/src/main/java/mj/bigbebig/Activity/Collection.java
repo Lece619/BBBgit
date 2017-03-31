0df294e46aaf4589d7e3c69a313f4c12579f2832
seoin <inguk920@naver.com>
2017-03-31 18:28:52 +0900

    animation anima

---
+++ app/src/main/java/mj/bigbebig/Activity/Collection.java
@@ -0,0 +1,21 @@
+package mj.bigbebig.Activity;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import mj.bigbebig.R;
+
+/**
+ * Created by Seo on 2017-03-31.
+ */
+
+public class Collection extends Activity {
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_collection);
+
+    }
+
+
+}
+
